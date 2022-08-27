7f9a7f288a5b19a92fce983c106f9e0402a6069c
ianisnotlan <ian990436@gmail.com>
2022-08-27 22:02:40 +0800

    built

--- src/src/prod.py
+++ src/src/prod.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOST')]
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(',')
 
 
 # Application definition
